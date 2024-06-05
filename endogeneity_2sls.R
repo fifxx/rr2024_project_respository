dc5796ca3bab3c107dbdfb7b4a4ac3cea7a3de86
Alexey Demchuk <a.demchuk@student.uw.edu.pl>
2024-06-05 16:58:15 +0300

    Added a final part of the research code to RMD

--- endogeneity_2sls.R
+++ endogeneity_2sls.R
@@ -5,12 +5,15 @@ library(ggplot2)
 library(ggrepel)
 library(broom)
 library(stargazer)
+library(lmtest)
+library(AER)
+library(ggrepel)
 
 df <- read_dta('https://github.com/QuantEcon/lecture-python/blob/master/source/_static/lecture_specific/ols/maketable1.dta?raw=true')
 
 ######## Endogeneity
 
-# Subset data to remove rows with missing values in logem4 and avexpr
+# Subsetting data to remove rows with missing values in logem4 and avexpr
 df1_subset2 <- df[complete.cases(df[c("logem4", "avexpr")]) , ]
 X <- df1_subset2$logem4
 y <- df1_subset2$avexpr
@@ -20,15 +23,14 @@ labels <- df1_subset2$shortnam
 ggplot(df1_subset2, aes(x = logem4, y = avexpr)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
-  geom_text(aes(x = logem4, y = avexpr, label = labels), hjust = 0, vjust = 0) +
+  geom_text_repel(aes(label = labels), hjust = 0, vjust = 0, , size = 3) +
   labs(title = "Figure 3: First-stage relationship between settler mortality and expropriation risk",
        x = "Log of Settler Mortality",
        y = "Average Expropriation Risk 1985-95") +
   theme_bw()
 
-# 2SLS regression
-library(lmtest)
 
+# 2SLS regression
 # First stage
 df4 <- read_dta('https://github.com/QuantEcon/lecture-python/blob/master/source/_static/lecture_specific/ols/maketable4.dta?raw=true')
 
@@ -45,7 +47,7 @@ summary(model_fs)
 
 ## Second stage
 # Adding predicted avexpr as a new variable
-df4$predicted_avexpr <- predict(model_fs, newdata = df4)  # Use predict from the model
+df4$predicted_avexpr <- predict(model_fs, newdata = df4)
 
 # Second-stage regression with predicted avexpr
 model_ss <- lm(logpgp95 ~ const + predicted_avexpr, data = df4)
@@ -53,10 +55,6 @@ summary(model_ss)
 
 # The second-stage regression results give us an unbiased and consistent estimate
 # of the effect of institutions on economic outcomes.
-
-# Correctly estimate a 2SLS regression
-# using the ivreg function from the AER package
-library("AER")
 
 # Second-stage regression using Two-Stage Least Squares (2SLS)
 # the default covariance estimator for the ivreg function from the AER package
